6364e3e2b0e3758366ce855291343de1d7df4e6c
juan jose gonzalezs <juansgj.91@gmail.com>
2021-04-05 19:14:33 +0200

    fin seccion

---
+++ flutter-socket.io-server-1.0.0/routes/mensajes.js
@@ -0,0 +1,22 @@
+/*
+path: /api/mensajes
+ */
+
+/**
+ path: api/usuarios
+ */
+
+
+ const {Router}  = require ('express');
+const { obtenerChat } = require('../controllers/mensajes');
+
+
+ const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+ const router = Router();
+ 
+ 
+ router.get('/:de',validarJWT, obtenerChat);
+ 
+ module.exports = router;
--- flutter-socket.io-server-1.0.0/sockets/socket.js
+++ flutter-socket.io-server-1.0.0/sockets/socket.js
@@ -1,12 +1,31 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const { usuarioConectado,usuarioDesconectado, grabarMensaje} = require('../controllers/socket')
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
+    const [valido,uid] = comprobarJWT(client.handshake.headers['x-token']);
+//verificar autenticacion
+    if(!valido) return client.disconnect();
+
+     usuarioConectado(uid);
+
+
+
+    client.join(uid);
+    //client.to(uid).emit('');
+    //escuchar del cliente el mensaje personal
+    client.on('mensaje-personal', (payload) =>{
+        //console.log(payload);
+        await grabarMensaje(payload);
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        //console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
